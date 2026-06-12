efbddb77a47f6e1c8d6ce005642a5424b157ab98
Desarrollo2 <desarrollo2@equatoroses.com>
2026-06-11 23:09:52 -0500

    Deploy automático

--- src/pages/Boleteria/NuevoBoletoPage.jsx
+++ src/pages/Boleteria/NuevoBoletoPage.jsx
@@ -1,6 +1,6 @@
 /* global qz */
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BusVisualizer } from './components/BusVisualizer';
 import { PasajerosGrid } from './components/PasajerosGrid';
