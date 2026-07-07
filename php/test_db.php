0d0b05e4cb2ee34d34da4e2d39380597758cb783
Desarrollo2 <desarrollo2@equatoroses.com>
2026-07-06 21:44:58 -0500

    Deploy automático

--- php/test_db.php
+++ php/test_db.php
@@ -1,11 +1,8 @@
 <?php
 require 'db.php';
 $conn = conexion();
-$res = mysqli_query($conn, "SHOW CREATE VIEW guia_nota_venta_lista");
-$row = mysqli_fetch_assoc($res);
-echo $row['Create View'] . "\n\n";
-
-$res2 = mysqli_query($conn, "SHOW CREATE VIEW guia_lista");
-$row2 = mysqli_fetch_assoc($res2);
-echo $row2['Create View'] . "\n";
+$res = mysqli_query($conn, "SELECT id_fksucursal_boleto FROM boletos ORDER BY id_boleto DESC LIMIT 5");
+while($row = mysqli_fetch_assoc($res)) {
+    echo "id_fksucursal_boleto: " . $row['id_fksucursal_boleto'] . "\n";
+}
 ?>
