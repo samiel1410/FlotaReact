59658f32a950be5d38b38afb4ba03c407b1fec80
Desarrollo2 <desarrollo2@equatoroses.com>
2026-07-23 12:33:17 -0500

    Deploy automático

--- php/negocioXmlFacturaData.php
+++ php/negocioXmlFacturaData.php
@@ -3,12 +3,8 @@ require_once __DIR__ . "/armarXml.php";
 $ver = new meotodoXml();
 $id_factura = $_GET['id_factura'];
 $var = $ver->armarXml($id_factura);
-$datosEmpresa = $ver->obtenerEmpresaInfo();
 
-$rucEmpresa = !empty($datosEmpresa[0]['ruc_empresa']) ? $datosEmpresa[0]['ruc_empresa'] : '';
-$passwordP12 = !empty($datosEmpresa[0]['password_p12']) ? decrypt_db_data($datosEmpresa[0]['password_p12']) : '';
-
-// Guardar el XML en un archivo físico para logs/respaldo si se desea
+// Guardar el XML en un archivo físico
 $xmlFolder = __DIR__ . '/xml_facturas/';
 if (!is_dir($xmlFolder)) {
     mkdir($xmlFolder, 0777, true);
@@ -16,13 +12,12 @@ if (!is_dir($xmlFolder)) {
 $xmlFile = $xmlFolder . 'factura_' . $id_factura . '.xml';
 file_put_contents($xmlFile, $var['comprobante']);
 
-// Devolver el XML y datos de firma en formato JSON
+// Devolver el XML en formato JSON (igual que Boletos)
 header('Content-Type: application/json');
 echo json_encode([
     'success' => true,
     'xml' => $var['comprobante'],
-    'ruc' => $rucEmpresa,
-    'p12_password' => $passwordP12,
-    'clave_acceso' => $var['clave_acceso_factura']
+    'clave_acceso' => isset($var['clave_acceso_factura']) ? $var['clave_acceso_factura'] : null,
+    'p12_password' => isset($var['p12_password']) ? $var['p12_password'] : null
 ]);
 ?>
